722a90ff1f21f2c852619655330f27a435103c54
Jose <josse180804@gmail.com>
2026-05-19 14:18:29 +0200

    Tracking movil asociado al incidente activo

--- backend/emergency/apps/api/serializers/track_serializers.py
+++ backend/emergency/apps/api/serializers/track_serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from emergency.apps.core.models import TrackPoint
+from emergency.apps.core.models import IncidentMember, TrackPoint
 
 
 class TrackPointSerializer(serializers.ModelSerializer):
@@ -29,6 +29,15 @@ class TrackPointCreateSerializer(serializers.ModelSerializer):
             'altitude', 'speed', 'recorded_at', 'incident'
         ]
 
+    def _get_active_incident_for_user(self, user):
+        membership = (
+            IncidentMember.objects.select_related('incident')
+            .filter(user=user, is_active=True, incident__status='OPEN')
+            .order_by('-joined_at')
+            .first()
+        )
+        return membership.incident if membership else None
+
     def create(self, validated_data):
         from django.contrib.gis.geos import Point
         latitude = validated_data.pop('latitude')
@@ -37,4 +46,8 @@ class TrackPointCreateSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         if request and getattr(request, 'user', None) and request.user.is_authenticated:
             validated_data['user'] = request.user
+            if not validated_data.get('incident'):
+                active_incident = self._get_active_incident_for_user(request.user)
+                if active_incident:
+                    validated_data['incident'] = active_incident
         return TrackPoint.objects.create(location=location, **validated_data)
